16157b0a66ad8284de9bb63c044de75b48c2a152
Davidnino21 <davidnino_21@yahoo.com>
2025-01-29 11:57:25 -0800

    added pokemon stats

--- script.js
+++ script.js
@@ -17,28 +17,41 @@ function getPokemons() {
         });
 }
 
+function getPokemonDetails(url, element) {
+    fetch(url)
+    .then(function(resp) {
+        return resp.json()
+    })
+    .then(function (pokemon) {
+        const {abilities, stats, types } = pokemon;
+        renderDetails(abilities, stats, element, types)
+    })
+    .catch(function (error) {
+        console.error("Error fetching data", error)
+    })
+}
+
 function renderPokemons(pokemons) {
     pokeCard.innerHTML = ""
     pokemons.forEach(function (pokemon, index) {
         const card = document.createElement("div");
         card.classList.add("card");
+        card.setAttribute("data-url", pokemon.url)
         card.style.width = "20rem";
         const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.url.split("/")[6]}.svg`;
-        console.log(imgSrc)
 
         card.innerHTML = `
          <div class="card-face card-front">
                     <img src="${imgSrc}" class="card-img-top" alt="${pokemon.name}">
                     <div class="card-body">
                         <h5 class="card-title">${pokemon.name}</h5>
-                        <p class="card-text">A Pokémon from the Pokémon API.</p>
                         <button class="btn btn-primary viewBtn">View Details</button>
                     </div>
                 </div>
                 <div class="card-face card-back">
                     <div class="card-body">
                         <h5 class="card-title">${pokemon.name}</h5>
-                        <p class="card-text">Details about ${pokemon.name}</p>
+                        <div class="card-details">Details about ${pokemon.name}</div>
                         <button class="btn btn-primary backBtn">Back</button>
                     </div>
                 </div>
@@ -49,14 +62,46 @@ function renderPokemons(pokemons) {
     document.querySelectorAll(".viewBtn").forEach(btn => {
         btn.addEventListener("click", (e) => {
             e.target.closest(".card").classList.toggle("is-flipped")
+            e.target.closest(".card").querySelector(".card-front").style.display = "none"
+            e.target.closest(".card").querySelector(".card-back").style.display = "block"
+           getPokemonDetails(e.target.closest(".card").dataset.url, e.target.closest(".card").querySelector(".card-details"))
         })
     })
     document.querySelectorAll(".backBtn").forEach(btn => {
         btn.addEventListener("click", (e) => {
             e.target.closest(".card").classList.toggle("is-flipped")
+             e.target.closest(".card").querySelector(".card-front").style.display = "block"
+            e.target.closest(".card").querySelector(".card-back").style.display = "none"
         })
     })
 
+}
+
+function renderDetails(abilities, stats, element, types) {
+    console.log(element)
+    element.innerHTML = ""
+    const abilitiesList = document.createElement("ul")
+    abilities.forEach(function(ability) {
+        const abilityItem = document.createElement("li")
+        abilityItem.textContent = ability.ability.name
+        abilitiesList.appendChild(abilityItem)
+    })
+    const statsList = document.createElement("ul")
+    stats.forEach(function(stat){
+        const statItem = document.createElement("li")
+        statItem.textContent = `${stat.stat.name} ${stat.base_stat}`
+        statsList.appendChild(statItem)
+    })
+    const typesList = document.createElement("ul")
+    types.forEach(function(type){
+        const typeItem = document.createElement("li")
+        typeItem.textContent = type.type.name
+        typesList.appendChild(typeItem)
+    })
+
+    element.appendChild(abilitiesList)
+    element.appendChild(statsList)
+    element.appendChild(typesList)
 }
 
 
